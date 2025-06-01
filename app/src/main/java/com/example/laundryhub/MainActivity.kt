7ad6cdea61cce60eb41793e1fc199cd041428a27
arvinr11 <roeslimarvin@gmail.com>
2025-06-01 16:14:52 +0700

    Add Home Page

--- app/src/main/java/com/example/laundryhub/MainActivity.kt
+++ app/src/main/java/com/example/laundryhub/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.laundryhub.Page.HomePage
 import com.example.laundryhub.Page.TransactionPage
 
 class MainActivity : AppCompatActivity() {
@@ -16,9 +17,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var btn = findViewById<Button>(R.id.btn)
+        var btnHome = findViewById<Button>(R.id.btn_home)
 
         btn.setOnClickListener(){
             val intent = Intent(this, TransactionPage::class.java)
+            startActivity(intent)
+        }
+
+        btnHome.setOnClickListener(){
+            val intent = Intent(this, HomePage::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/laundryhub/Page/HomePage.kt
+++ app/src/main/java/com/example/laundryhub/Page/HomePage.kt
@@ -1,6 +1,12 @@
 package com.example.laundryhub.Page
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +14,67 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.laundryhub.R
 
 class HomePage : AppCompatActivity() {
+
+    private lateinit var tvNomorAntrian: TextView
+    private lateinit var tvAntrianUser: TextView
+    private lateinit var btnAmbilAntrian: Button
+
+    private var nomorAntrianSekarang = 1
+    private val handler = Handler(Looper.getMainLooper())
+    private val updateInterval: Long = 10000
+
+    private var nomorYangDiambilUser: Int? = null
+
+    private val runnable = object : Runnable {
+        override fun run() {
+            nomorAntrianSekarang++
+            tvNomorAntrian.text = "$nomorAntrianSekarang"
+            handler.postDelayed(this, updateInterval)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val tvUser: TextView = findViewById(R.id.tv_user)
+        tvNomorAntrian = findViewById(R.id.tv_nomor_antrian)
+        tvAntrianUser = findViewById(R.id.tv_antrian_user)
+        btnAmbilAntrian = findViewById(R.id.btn_ambil_antrian)
+
+//        ambil username saat login
+//        tvUser.text = "Welcome,  ${user}"
+
+        tvAntrianUser.visibility = View.GONE
+        tvNomorAntrian.text = "0"
+
+        btnAmbilAntrian.setOnClickListener {
+            if(nomorYangDiambilUser != null) {
+                Toast.makeText(this, "Anda sudah mengambil nomor: $nomorYangDiambilUser", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val nomorUntukDiambil = nomorAntrianSekarang
+
+            nomorYangDiambilUser = nomorUntukDiambil
+
+            tvAntrianUser.text = "Nomor Antrian Anda: $nomorYangDiambilUser"
+            tvAntrianUser.visibility = View.VISIBLE
+
+            handler.removeCallbacks(runnable)
+            nomorAntrianSekarang++
+            tvNomorAntrian.text = "$nomorAntrianSekarang"
+            handler.postDelayed(runnable, updateInterval)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.post(runnable)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
     }
 }
