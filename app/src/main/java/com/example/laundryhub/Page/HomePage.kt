f2820f3611f41a3e415d7e8b0f343d1e2e5c4bb9
arvinr11 <roeslimarvin@gmail.com>
2025-06-01 16:16:53 +0700

    Add Home Page

--- app/src/main/java/com/example/laundryhub/Page/HomePage.kt
+++ app/src/main/java/com/example/laundryhub/Page/HomePage.kt
@@ -7,10 +7,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.laundryhub.R
 
 class HomePage : AppCompatActivity() {
